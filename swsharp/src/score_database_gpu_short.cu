0566f946a7615e8cd65ed535ec8941ceefd7b87a
mkorpar <mkorpar@gmail.com>
2014-04-20 15:40:28 +0200

    remove unused variable warnings

--- swsharp/src/score_database_gpu_short.cu
+++ swsharp/src/score_database_gpu_short.cu
@@ -566,10 +566,12 @@ static ShortDatabase* createDatabase(Chain** database, int databaseLen,
         gpuDatabases[i].scores = scoresGpu;
         gpuDatabases[i].hBus = hBusGpu;
         
+#ifdef DEBUG
         size_t memory = offsetsSize + 2 * lengthsSize + sequencesSize + 
             indexesSize + scoresSize + hBusSize;
 
         LOG("Short database using %.2lfMBs on card %d", memory / 1024.0 / 1024.0, card);
+#endif
     }
     
     //**************************************************************************
